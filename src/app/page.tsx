b71deb8d9068c55221955484f513091be1e8cb72
Nur Alam <nuralam.rsc@gmail.com>
2024-10-14 23:07:43 +0600

    FIX: added space between car card and show more button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default async function Home({ searchParams }) {
 
         {!isEmptyData ? (
           <section>
-            <div className="home__cars-wrapper">
+            <div className="home__cars-wrapper mb-2">
               {allCars?.map((car) => (
                 <CarCard key={car} car={car} />
               ))}
